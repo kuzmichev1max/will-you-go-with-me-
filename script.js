4e2f9c2e13725961c316b0a8d5ba9c4005832f0d
kuzmichev1max <128485351+kuzmichev1max@users.noreply.github.com>
2023-11-28 04:09:12 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -3,21 +3,27 @@ const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
+const calendarLink = document.querySelector(".calendar-link");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Увидимся с тобой 29.11";
-  gif.src =
-    "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+    console.log("dsadasd");
+    question.innerHTML = "Увидимся с тобой 29.11";
+    gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+
+    yesBtn.classList.add("is-hidden");
+    calendarLink.classList.remove("is-hidden");
 });
 
+const btnGroup = document.querySelector(".btn-group");
+
 noBtn.addEventListener("mouseover", () => {
-  const noBtnRect = noBtn.getBoundingClientRect();
-  const maxX = window.innerWidth - noBtnRect.width;
-  const maxY = window.innerHeight - noBtnRect.height;
+  const maxX = window.innerWidth - noBtn.clientWidth;
+  const maxY = window.innerHeight - noBtn.clientHeight;
 
   const randomX = Math.floor(Math.random() * maxX);
   const randomY = Math.floor(Math.random() * maxY);
 
+  noBtn.style.position = "fixed";
   noBtn.style.left = randomX + "px";
   noBtn.style.top = randomY + "px";
 });
